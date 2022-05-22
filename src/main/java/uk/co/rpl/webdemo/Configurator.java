1b78ac306c438d72ccd1f3537e5c932c69755236
philip <philip.robinson@r-pl.co.uk>
2022-05-22 10:30:56 +0100

    split config into two files

--- src/main/java/uk/co/rpl/webdemo/Configurator.java
+++ src/main/java/uk/co/rpl/webdemo/Configurator.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -14,6 +15,7 @@ import org.springframework.web.client.RestTemplate;
  * @author philip
  */
 @Configuration
+@PropertySource("classpath:extra.properties")
 @Getter
 @Slf4j
 public class Configurator {
